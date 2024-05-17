90b243902e0648fd95ccd956bda01283a57f91d8
Janosh <popret@gmail.com>
2024-05-18 00:01:06 +0200

    Removed Garbage Code

--- transfer.py
+++ transfer.py
@@ -1,32 +1,5 @@
 import re
-
-def apply_replacements(content, replacements):
-    for pattern, replacement in replacements:
-        content = re.sub(pattern, replacement, content)
-    return content
-
-def load_replacements(file_path):
-    replacements = []
-    with open(file_path, 'r') as file:
-        for line in file:
-            pattern, replacement = line.strip().split(' -> ')
-            replacements.append((pattern, replacement))
-    return replacements
-
 def main(original, tagged, output_file):
-    with open(original, 'r') as file:
-        content = file.read()
-    
-    replacements = load_replacements(patterns_file)
-    
-    modified_content = apply_replacements(content, replacements)
-    
-    with open(output_file, 'w') as file:
-        file.write(modified_content)
-    
-    print(f"Modified content has been written to {output_file}")
-
-def new(original, tagged, output_file):
     matchname = re.compile(r'^\d ([^\(]*)\(.*$')
     matchnamesandtags = re.compile(r'^\d ([^\(]*)\([^#]*#(.*)$', re.MULTILINE)
     
@@ -50,5 +23,5 @@ if __name__ == "__main__":
     original = 'deck1.txt'
     tagged = 'deck2.txt'
     combined = 'combined.txt'
-    new(original, tagged, combined)
+    main(original, tagged, combined)
 
